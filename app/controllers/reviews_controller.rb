75cc28f62c7d397a29eeda068d70bad4a98fbeea
jonathankim407 <jonathankim407@gmail.com>
2017-06-14 16:17:44 -0400

    add create method and review_params method in reviews controller, add field text for name to product show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,25 @@
 class ReviewsController < ApplicationController
 
+  def index
+  end
+
+  def show
+  end
+
   def new
   end
 
   def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+
+    if @review.save
+      flash[:notice] = "Your review has been successfully created!"
+      redirect_to products_path
+    else
+      render new_product_path
+      # render :new
+    end
   end
 
   def edit
@@ -15,10 +31,8 @@ class ReviewsController < ApplicationController
   def destroy
   end
 
-  def show
-  end
-
-  def index
+  def review_params
+    params.require(:review).permit(:name, :comment)
   end
 
 end
