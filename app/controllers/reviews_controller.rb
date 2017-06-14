ff6b96e0e90f69488b573edacf77ed406a0885a8
jonathankim407 <jonathankim407@gmail.com>
2017-06-14 15:35:05 -0400

    add form_for to product show page, add to product controller show method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,26 +1,24 @@
 class ReviewsController < ApplicationController
 
-def new
-end
+  def new
+  end
 
-def create
-end
+  def create
+  end
 
-def edit
-end
+  def edit
+  end
 
-def update
-end
+  def update
+  end
 
-def destroy
-end
+  def destroy
+  end
 
-def show
-end
+  def show
+  end
 
-def index
-end
-
-
+  def index
+  end
 
 end
